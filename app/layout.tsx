10a372a09a7110e9ffb59f8e5ff4c2a9bf2ba567
hima <ebrahumahmed2003@gmail.com>
2025-12-10 12:33:28 +0200

    working with the RSC

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,10 +1,17 @@
- 
+import NavLink from "./components/NavLink";
+export const metadata={
+  title:"The Wild Oasis",
+}
 export default function Layout({ children }: { children: React.ReactNode }) {
   return (
     <html>
       <body>
-        {children}
+        <header>
+          <img src="logo.png" />
+        <NavLink />
+        </header>
+        <main>{children}</main>
       </body>
     </html>
-  )
+  );
 }
